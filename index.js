fc984599dd32b31f9e0a5d1aaa52e17786431213
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-09-16 10:41:54 -0300

    Add: simple emit / receive functions

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let express = require("express");
 let app = express();
-let http = require("http").createServer(app);
-let io = require("socket.io")(http);
+let http = require("http").createServer(app); // Create http server
+let io = require("socket.io")(http); // connect http server in socket.io
 
 app.set("view engine", "ejs");
 
-io.on("connection",(socket) => {
+io.on("connection",(socket) => { // Create the socket.io connection
 
+    socket.on("disconnect", () => { // when a client(socket) disconnet from server
+        console.log(`${socket.id} - Discconnected from server`);
+    })
     
+    socket.on("sendMsg", (data) => { // whent the event "sendMsg" is captured
+        socket.emit("showMsg", data); // send a new event called "showMsg"
+        console.log(data);
+    })
 })
 
 app.get("/", (req,res) => {
@@ -17,5 +24,5 @@ app.get("/", (req,res) => {
 
 
 http.listen(3000,() => {
-    console.log("App running on port: 3000");
+    console.log("App running on: http://localhost:3000");
 })
